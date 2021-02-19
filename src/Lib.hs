d0dac2a24f8e713149542aded5629444e142da47
DidierLatreille <didier.latreille@gmail.com>
2021-02-19 12:59:38 -0300

    aVerElPush

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,34 @@
-module Lib where
-import Text.Show.Functions
+module Tecnicas where
+type Objetivo = String
 
-laVerdad = True
+poderGolpe :: Int -> Int
+poderGolpe = ( 15 * )
+
+fortalezaObjetivo :: Objetivo -> Int
+fortalezaObjetivo = ( (2 *) . length )
+
+-- prototipofunciongolpe
+-- golpe :: Int -> Objetivo -> Int
+-- golpe horasEntrenamiento nombreObjetivo = div ( 15 * horasEntrenamiento ) ( ( (2 *) . length ) nombreObjetivo )
+
+-- mide la presion del golpe
+golpe :: Int -> Objetivo -> Int
+golpe horasEntrenamiento nombreObjetivo = div (poderGolpe horasEntrenamiento) (fortalezaObjetivo nombreObjetivo)
+
+horasGolpesNormalesConsecutivos :: Int
+horasGolpesNormalesConsecutivos = 240
+
+horasGomuGomuElephantGatling :: Int
+horasGomuGomuElephantGatling = 180 
+
+presionLimiteDeGomu :: Int
+presionLimiteDeGomu = 100
+
+objetivoDificil :: String -> Bool
+objetivoDificil = (presionLimiteDeGomu >).golpe horasGomuGomuElephantGatling
+
+focalizarObjetivo :: String -> String
+focalizarObjetivo = take 7
+
+accesibilidadObjetivo :: String -> Int
+accesibilidadObjetivo nombreObjetivo = div  (((golpe horasGolpesNormalesConsecutivos).focalizarObjetivo ) nombreObjetivo ) 10
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,31 @@
 import Test.Hspec
-import Lib
+import Tecnicas
+
+objetivo :: Objetivo
+objetivo = "bolsa de entrenamiento"
 
 main :: IO()
 main = hspec $ do
-   describe "Tests de prueba" $ do
-      it "la verdad y la verdad" $ do
-         laVerdad `shouldBe` True
+   describe "golpe" $ do
+      it "presion de golpes normales consecutivos sobre 'bolsa de entrenamiento' = 81" $ do
+         golpe horasGolpesNormalesConsecutivos "bolsa de entrenamiento" `shouldBe` 81
+
+      it "presion de gomu gomu elephant gatling sobre 'bolsa de entrenamiento' = 61" $ do
+         golpe horasGomuGomuElephantGatling "bolsa de entrenamiento" `shouldBe` 61
+
+      it "presion de golpe de 200 horas de entrenamiento sobre 'bolsa de entrenamiento' = 68" $ do
+         golpe 200 "bolsa de entrenamiento" `shouldBe` 68
+
+   describe "objetivoDificil" $ do
+      it "el objetivo 'bolsa de entrenamiento ES dificil' " $ do
+         objetivoDificil "bolsa de entrenamiento" `shouldBe` True
+      
+      it "el objetivo 'gatito' NO ES dificil" $ do
+         objetivoDificil "gatito" `shouldBe` False
+
+   describe "accesibilidadObjetivo" $ do
+      it "la accesibilidad de 'bolsa de entrenamiento' es = 25" $ do
+         accesibilidadObjetivo "bolsa de entrenamiento" `shouldBe` 25
+
+      it "la accesibilidad de 'gatito' es = 30" $ do
+         accesibilidadObjetivo "gatito" `shouldBe` 30
